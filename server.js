5099530df4d289ee584e215fe1dd88b3fa8860dd
SCA2 <tpryan_1965@yahoo.com>
2017-05-30 12:03:42 -0700

    add redirect

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(useragent.express());
+
+app.get('/', (req, res) => {
+  res.redirect('/api/whoami');
+});
+
 app.get('/api/whoami', (req, res) => {
-  // console.log(req.headers);
   var info = {
     ipaddress: req.ip.match(/\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$/)[0],
     language: req.headers['accept-language'].split(',')[0],
     software: req.useragent.os
   }
-  // console.log(info);
 
   res.json(info);
 });
